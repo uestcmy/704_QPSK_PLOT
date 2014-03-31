d6bc0430371bd42e76bc320f2bf3990bedeac7cb
uestcmy <369064521@qq.com>
2014-03-31 17:15:28 +0800

    qpsk3-original
    500 points in 2D

---
+++ hhqpsk.pro
@@ -0,0 +1,17 @@
+
+QT += opengl
+SOURCES += \
+    main.cpp \
+    biterrorrate.cpp \
+    ber.cpp \
+    qpsk3.cpp
+
+HEADERS += \
+    biterrorrate.h \
+    ber.h \
+    qpsk3.h
+
+FORMS += \
+    biterrorrate.ui \
+    ber.ui \
+    qpsk3.ui
---
+++ main.cpp
@@ -0,0 +1,44 @@
+
+
+#include <QApplication>
+
+#include <qpsk3.h>
+#include <ber.h>
+#include <biterrorrate.h>
+#include <GL/glut.h>
+
+
+
+
+#include <qfile.h>
+
+#include <QDebug>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <GL/glut.h>
+int main(int argc, char *argv[])
+{
+    QApplication app(argc,argv);
+
+/*
+    BitErrorRate w32;
+    w32.resize(500,450);
+    w32.show();
+    w32.setWindowTitle("Bit Error Rate");
+*/
+
+    QPSK3 w3;
+    w3.resize(600,450);
+    w3.show();
+    w3.setWindowTitle("QPSK Star ( Port:7005)");
+
+
+
+    return app.exec();
+
+}
+
+
